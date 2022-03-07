244e153a77ced1efc62f2ba8da68ef9684c16f5a
keith-pineo <k.pineo@dal.ca>
2022-03-07 18:47:16 -0400

    Implemented some easy to use wrappers for Node's crypto module, as well as a route to test their functioning.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 /*
-Primary server file for getting things initialized.
+Primary server file for running things and handling requests.
 
 Author(s): Keith Pineo
 */
 console.log("Loading server for Iron Man 2.")
+//Node Modules
 const express = require('express');
+
+//Modules written for this project
 const config = require('./config.js');
+const cryptography = require('./cryptography.js');
 
 const app = express();
 app.use(express.json()); //Allow JSON responses to be sent back.
@@ -19,8 +23,31 @@ app.get('/is_the_server_responding_to_requests', async (req, res) => {
     });
 });
 
+app.get('/encryption_test', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+    const hashedIP = cryptography.SHA3(ip);
+    const encryptedIP = cryptography.encryptTextAES256(ip);
+    const decryptedIP = cryptography.decryptTextAES256(encryptedIP);
+
+    const JSONObject = {'yourIPInSomeJSON':ip,'justARandomNumber':Math.random()};
+
+    const encryptedJSON = cryptography.encryptJSONAES256(JSONObject);
+    const decryptedJSON = cryptography.decryptJSONAES256(encryptedJSON);
+
+    res.json({
+        'ipAddress': ip,
+        hashedIP,
+        encryptedIP,
+        decryptedIP,
+        'JSONEncryptTest':JSONObject,
+        encryptedJSON,
+        decryptedJSON
+    });
+});
+
 app.listen(config.port);
 
 console.log(config)
 
-console.log(`Server load complete. The server is now listening for requests on port ${config.port}.`)
+console.log(`Server load complete. The server is now listening for requests on port ${config.port}.`);
