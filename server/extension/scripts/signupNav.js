418eafe915bfe4853924935582cecb87349a2b67
js887505 <js887505@dal.ca>
2022-03-14 22:45:01 -0300

    Fixing layout and IDs of form.

--- server/extension/scripts/signupNav.js
+++ server/extension/scripts/signupNav.js
@@ -1,9 +1,10 @@
 var mainSection = document.getElementById("signupOrLoginSection");
-var buttonsDiv = document.getElementById("signupOrLoginButtons");
+var loginORsignupDiv = document.getElementById("signupOrLoginButtons");
 var signupDiv = document.getElementById("signupDiv");
 var loginDiv = document.getElementById("loginDiv");
 
 document.getElementById("signupButton").addEventListener("click", signupSelected);
+document.getElementById("noAccountSignupButton").addEventListener("click", signupSelected);
 document.getElementById("loginButton").addEventListener("click", loginSelected);
 document.getElementById("signupBackToMain").addEventListener("click", backToMain);
 document.getElementById("loginBackToMain").addEventListener("click", backToMain);
@@ -12,17 +13,19 @@ signupDiv.style.display = "none";
 loginDiv.style.display = "none";
 
 function signupSelected(){
-  buttonsDiv.style.display = "none";
+  loginORsignupDiv.style.display = "none";
+  loginDiv.style.display = "none";
   signupDiv.style.display = "inline-block";
 }
 
 function loginSelected(){
-  buttonsDiv.style.display = "none";
+  loginORsignupDiv.style.display = "none";
+  signupDiv.style.display = "none";
   loginDiv.style.display = "inline-block";
 }
 
 function backToMain(){
   signupDiv.style.display = "none";
   loginDiv.style.display = "none";
-  buttonsDiv.style.display = "inline-block";
+  loginORsignupDiv.style.display = "inline-block";
 }
