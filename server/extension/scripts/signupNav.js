cde6eb55b0f647d353d0e3978dbd33a8e7e08ed8
Ty Fetinko <ty667216@dal.ca>
2022-03-15 22:13:07 -0300

    added show acct to pop up but doesnt change by usr

--- server/extension/scripts/signupNav.js
+++ server/extension/scripts/signupNav.js
@@ -2,14 +2,18 @@ var mainSection = document.getElementById("signupOrLoginSection");
 var buttonsDiv = document.getElementById("signupOrLoginButtons");
 var signupDiv = document.getElementById("signupDiv");
 var loginDiv = document.getElementById("loginDiv");
+var showAcct = document.getElementById("showAcct");
+
 
 document.getElementById("signupOption").addEventListener("click", signupSelected);
 document.getElementById("loginOption").addEventListener("click", loginSelected);
 document.getElementById("signupBackToMain").addEventListener("click", backToMain);
 document.getElementById("loginBackToMain").addEventListener("click", backToMain);
+document.getElementById("loginButton").addEventListener("click",dislayAcct);
 
 signupDiv.style.display = "none";
 loginDiv.style.display = "none";
+showAcct.style.display = "none";
 
 function signupSelected(){
   buttonsDiv.style.display = "none";
@@ -25,4 +29,10 @@ function backToMain(){
   signupDiv.style.display = "none";
   loginDiv.style.display = "none";
   buttonsDiv.style.display = "inline-block";
+}
+function dislayAcct(){
+
+  signupDiv.style.display = "none";
+  loginDiv.style.display = "none";
+  showAcct.style.display = "inline-block";
 }
