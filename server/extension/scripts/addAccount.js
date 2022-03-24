73984af365f15d2cb09bebb5e606c0530ab6a04d
keith-pineo <k.pineo@dal.ca>
2022-03-24 16:34:04 -0300

    Added show/hide password button and removed gibberish placeholder text.

--- server/extension/scripts/addAccount.js
+++ server/extension/scripts/addAccount.js
@@ -1,8 +1,20 @@
 document.getElementById("addAccountButton").addEventListener("click", addAccount);
+document.getElementById("togglePasswordButton").addEventListener("click", toggleShowPassword);
 
 function addAccount(){
   accounts.addNewAccount(document.getElementById('platformField').value, document.getElementById('usernameField').value, document.getElementById('passwordField').value);
   let accountsObj = JSON.stringify(accounts);
   localStorage.setItem('accounts', accountsObj)
   window.location.href = "../pages/showAccounts.html";
+}
+
+function toggleShowPassword() {
+  pwField = document.getElementById("passwordField");
+
+  if (pwField.type == "password") {
+    pwField.type = "text";
+  }
+  else {
+    pwField.type = "password";
+  }
 }
