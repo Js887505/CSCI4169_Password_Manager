d992aaa1c8131797f326c71d24fc1aff7bcbba4a
js887505 <js887505@dal.ca>
2022-03-24 17:09:16 -0300

    Added index.html page, and lock account functionality

---
+++ server/extension/scripts/login.js
@@ -0,0 +1,5 @@
+document.getElementById('loginButton').addEventListener("click", login);
+
+function login(){
+  window.location.href = "../pages/showAccounts.html";
+}
---
+++ server/extension/scripts/signupNav.js
@@ -0,0 +1,32 @@
+var mainSection = document.getElementById("signupOrLoginSection");
+var loginORsignupDiv = document.getElementById("signupOrLoginDiv");
+var signupDiv = document.getElementById("signupDiv");
+var loginDiv = document.getElementById("loginDiv");
+
+document.getElementById("noAccountSignupButton").addEventListener("click", signupSelected);
+document.getElementById("signupOption").addEventListener("click", signupSelected);
+document.getElementById("loginOption").addEventListener("click", loginSelected);
+document.getElementById("signupBackToMain").addEventListener("click", backToMain);
+document.getElementById("loginBackToMain").addEventListener("click", backToMain);
+
+signupDiv.style.display = "none";
+loginDiv.style.display = "none";
+showAcct.style.display = "none";
+
+function signupSelected(){
+  loginORsignupDiv.style.display = "none";
+  loginDiv.style.display = "none";
+  signupDiv.style.display = "inline-block";
+}
+
+function loginSelected(){
+  loginORsignupDiv.style.display = "none";
+  signupDiv.style.display = "none";
+  loginDiv.style.display = "inline-block";
+}
+
+function backToMain(){
+  signupDiv.style.display = "none";
+  loginDiv.style.display = "none";
+  loginORsignupDiv.style.display = "inline-block";
+}
