a42ba4c807fd7f69dbf09bd56e4f11f153ace367
keith-pineo <k.pineo@dal.ca>
2022-03-31 17:46:02 -0300

    Clean up of main branch for final submission.
    Server files that went unused in the end have been moved to the server branch.
    
    Updated a few entries in the extension manifest.json

--- server/login.js
+++
@@ -1,8 +0,0 @@
-const config = require('./config.js');
-const cryptography = require('./cryptography.js');
-const app = require('./server.js');
-
-app.post('/login_data', function(req, res) {
-  console.log(req);
-  
-});
